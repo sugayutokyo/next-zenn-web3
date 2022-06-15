02af6699cd4803d946d10e8575b9fd8aabdb66fb
sugayutokyo <mygit_yutasugao20171126@yahoo.co.jp>
2022-06-15 13:05:20 +0900

    getAccounts

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,19 @@ import { ZennCoin } from '../types/abi/ZennCoin';
 
 // プロバイダの設定
 const web3 = new Web3(new Web3.providers.HttpProvider(`http://127.0.0.1:7545`));
-const ABI = ZCContract.abi as any as AbiItem;
 
 // コントラクトのアドレス
 const address = '0x7D4d7a0da0e8e1Dc90a86fDB82882a94190d89D6';
 
+const ABI = ZCContract.abi as any as AbiItem;
 // コントラクトのインスタンス
 const contract = new web3.eth.Contract(ABI, address) as unknown as ZennCoin;
 
 const Home: NextPage = () => {
+  (async () => {
+    const accountsWeb3 = await web3.eth.getAccounts();
+    console.log(accountsWeb3);
+  })();
   return <div></div>;
 };
 
